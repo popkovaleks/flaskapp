ade33635dbd079cf8dc15e7f8027053e0b685532
Aleksandr <rediculus667@gmail.com>
2021-06-15 18:50:56 +0500

    add user page function

--- app/routes.py
+++ app/routes.py
@@ -62,3 +62,13 @@ def registration():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template("registration.html", title = title, form = form)
+
+@APP.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
