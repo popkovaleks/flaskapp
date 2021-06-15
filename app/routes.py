d16cc4cb925ea752600ffdf3b9dfb3161bdc220d
Aleksandr <rediculus667@gmail.com>
2021-06-15 14:11:47 +0500

    processing next page on login page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm, RegistrationForm
 from app import APP
@@ -36,7 +37,10 @@ def login():
             flash('Invalid username or password')
             return(redirect(url_for('login')))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
         
     return render_template('login.html', title='Sign In', form=form)
 
