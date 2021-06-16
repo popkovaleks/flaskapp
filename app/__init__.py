f4b5fd1bb9aaa002f4f1c52ed3b71c08fbf18c7d
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-16 18:02:20 +0500

    log file handler

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+import logging
+import os
+
 
 APP = Flask(__name__)
 APP.config.from_object(Config)
@@ -12,3 +17,31 @@ login = LoginManager(APP)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+if not APP.debug:
+    if APP.config['MAIL_SERVER']:
+        auth = None
+        if APP.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if APP.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        APP.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    APP.logger.addHandler(file_handler)
+
+    APP.logger.setLevel(logging.INFO)
+    APP.logger.info('Microblog startup')
