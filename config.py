cbcdb2259b59df86a611d77b31adbf2123509251
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 17:29:26 +0500

    add posts per page

--- config.py
+++ config.py
@@ -12,4 +12,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['rediculus667@gmail.com']
+    ADMINS = ['popkov.aleks.v@yandex.ru']
+
+    POSTS_PER_PAGE =3 
