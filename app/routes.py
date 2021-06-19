92a615ddf95ea2aac8d2d0858ea8f2a4b267a5fa
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 23:18:10 +0500

    add reset_password_request

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,11 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_migrate import current
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequstForm
 from app import APP, db
 from app.models import User, Post
 from datetime import datetime
+from app.email import send_password_reset_email
 
 @APP.route('/', methods=['GET', 'POST'])
 @APP.route('/index', methods=['GET', 'POST'])
@@ -146,3 +147,17 @@ def explore():
     next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+@APP.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_requst():
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
+
+    form = ResetPasswordRequstForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password', form=form)
