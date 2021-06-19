2df9d6e9ea648cef3f80e909f1a98c1e3bbb8068
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 16:18:36 +0500

    add post for to index

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms.widgets.core import TextArea
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -43,3 +44,8 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+
+    submit = SubmitField('Post')
