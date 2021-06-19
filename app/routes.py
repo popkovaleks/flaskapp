264e74067243f92c58a4316a706399dcbd7db2d4
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 20:15:04 +0500

    add pages for user profile

--- app/routes.py
+++ app/routes.py
@@ -70,10 +70,13 @@ def registration():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, APP.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     
-    posts = Post.query.filter_by(user_id = current_user.id).order_by(Post.timestamp.desc())
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts, form=form, next_url=next_url, prev_url=prev_url)
 
 @APP.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
