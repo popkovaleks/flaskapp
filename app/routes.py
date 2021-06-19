3b53a9b23f83bdc2fcf5bf9e586e0bdc9cd00bf3
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 16:49:38 +0500

    show actual posts of user from db

--- app/routes.py
+++ app/routes.py
@@ -76,11 +76,8 @@ def registration():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-
+    
+    posts = Post.query.filter_by(user_id = current_user.id).order_by(Post.timestamp.desc())
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
 
