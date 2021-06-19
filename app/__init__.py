37c1f160c5ae15a22cf70e55c409c13a9f7468de
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-19 22:58:15 +0500

    add email sending

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 import logging
@@ -15,21 +16,22 @@ db = SQLAlchemy(APP)
 migrate = Migrate(APP, db)
 login = LoginManager(APP)
 login.login_view = 'login'
+mail = Mail(APP)
 
 from app import routes, models, errors
 
 if not APP.debug:
     if APP.config['MAIL_SERVER']:
         auth = None
-        if APP.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        if APP.config['MAIL_USERNAME'] or APP.config['MAIL_PASSWORD']:
+            auth = (APP.config['MAIL_USERNAME'], APP.config['MAIL_PASSWORD'])
         secure = None
         if APP.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            mailhost=(APP.config['MAIL_SERVER'], APP.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            toaddrs=APP.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         APP.logger.addHandler(mail_handler)
