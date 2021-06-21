5e44990845fed8ccfc436f250c7e68b34b579fa3
Aleksandr <popkov.aleks.v@yandex.ru>
2021-06-22 01:19:47 +0500

    change email config and reset password page correction

--- config.py
+++ config.py
@@ -7,11 +7,18 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLACHEMY_TRACK_MODIFICATIONS= False
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['popkov.aleks.v@yandex.ru']
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'rediculus667@gmail.com'
+    MAIL_PASSWORD = '*'
+    MAIL_DEBUG = True
 
     POSTS_PER_PAGE = 25 
